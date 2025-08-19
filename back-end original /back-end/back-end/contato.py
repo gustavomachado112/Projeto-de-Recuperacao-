73e031d5c11819ebb7c9dd5763b5462e6a2ffb0f
gustavomachado112 <159536397+gustavomachado112@users.noreply.github.com>
2025-08-18 21:06:28 -0300

    Delete back-end original /back-end/back-end directory

--- back-end original /back-end/back-end/contato.py
+++
@@ -1,9 +0,0 @@
-import random
-
-print("bem vindo ao contato!!")
-
-nome = input("Digite seu nome: ")
-email = input("Digite seu email: ")
-mensagem = input("Digite sua mensagem: ")
-
-print("Sua mensagem foi enviada!! Espere retorno.")
